2ea45f9d6816c1dc6e445cbf79153a8ae56940df
Joey-Hanna <joey.martin.de.lange@gmail.com>
2022-12-09 20:11:13 +0100

    ukol 7 - definovani promenne currentURL v controlleru

--- src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UrlPathHelper;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 
 @Controller
@@ -37,6 +41,20 @@ public class PostController {
     @GetMapping("/vyber")
     public String vyber() {
         return "vyber";
+    }
+
+    @ModelAttribute("currentURL")
+    public String currentURL(HttpServletRequest request) {
+        UrlPathHelper helper = new UrlPathHelper();
+        return UriComponentsBuilder
+                .newInstance()
+                .path(helper.getOriginatingRequestUri(request))
+                .query(helper.getOriginatingQueryString(request))
+                .replaceQueryParam("size")
+                .replaceQueryParam("page")
+                .replaceQueryParam("sort")
+                .build()
+                .toString();
     }
 }
 
